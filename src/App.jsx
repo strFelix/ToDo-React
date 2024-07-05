7159dd4d84bc817b3f478a8717d45539a5f9f5c7
Lucas Felix <Lucas@glide.com.br>
2024-07-05 08:58:26 -0300

    feat: mapping todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
-import { useState } from 'react'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState(
+    // redenring the todos
+    [
+      { id: 1, text: "Learn React", category: "Trabalho", isCompleted: false },
+      { id: 2, text: "Learn JS", category: "Estudos", isCompleted: false },
+      { id: 3, text: "Improve C#", category: "Pessoal", isCompleted: false },
+    ]
+  );
 
   return (
-    <> 
-    <p>Hello, React!</p>
-    </>
-  )
+    <div className="app">
+      <h1>Lista de tarefas</h1>
+      <div className="todo-list">
+        {todos.map(
+          (
+            // mapping the todos
+            todo 
+          ) => (
+            <div className="todo">
+              <div className="content">
+                <p>{todo.text}</p> 
+                <p className="category">({todo.category})</p>
+              </div>
+              <div>
+                <button>Completar</button>
+                <button>X</button>
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
